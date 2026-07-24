217104e26ad3b538642edceebcc71845e3ffc67e
ravikumarpernapati-beep <ravikumarpernapati@gmail.com>
2026-07-24 16:54:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import os
+import requests
 
+UPSTOX_ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+headers = {
+    "Accept": "application/json",
+    "Authorization": f"Bearer {UPSTOX_ACCESS_TOKEN}"
+}
+
+url = "https://api.upstox.com/v3/market-quote/quotes?instrument_key=BSE_INDEX|SENSEX"
+
+response = requests.get(url, headers=headers)
+
+print(response.status_code)
+print(response.text)
