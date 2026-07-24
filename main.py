191fe5b9503e28b6f89ece26f148e16825ff8a25
ravikumarpernapati-beep <ravikumarpernapati@gmail.com>
2026-07-24 17:48:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
 
 headers = {
     "Accept": "application/json",
+    "Content-Type": "application/json",
     "Authorization": f"Bearer {ACCESS_TOKEN}"
 }
 
 instrument_key = "BSE_INDEX|SENSEX"
 
-url = f"https://api.upstox.com/v2/historical-candle/{instrument_key}/5minute"
+url = f"https://api.upstox.com/v3/historical-candle/intraday/{instrument_key}/minutes/5"
 
 response = requests.get(url, headers=headers)
 
